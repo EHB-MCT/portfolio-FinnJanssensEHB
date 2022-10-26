6038c14a938a3f2f45c808f303fd204d7f21d747
finnjanssens <finnjanssensfj@gmail.com>
2022-10-26 17:39:58 +0200

    Add fetching from delijn API and calculation of delay

--- portfolio-api/src/index.js
+++ portfolio-api/src/index.js
@@ -1,10 +1,61 @@
+require("dotenv").config();
+const axios = require("axios");
+
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = 7000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// const { MongoClient } = require("mongodb");
+
+// const url = process.env.MONGODB_URI;
+// const client = new MongoClient(url);
+
+// const dbName = process.env.MONGODB_DATABASE;
+
+// async function mongo() {
+//   await client.connect();
+//   console.log("Connected successfully to server");
+//   const db = client.db(dbName);
+//   const collection = db.collection(process.env.MONGODB_COLLECTION);
+// }
+
+app.get("/delijn", async (req, res) => {
+  let data = [];
+  axios
+    .get(
+      "https://api.delijn.be/DLKernOpenData/v1/beta/haltes/3/303020/real-time",
+      {
+        headers: {
+          "Ocp-Apim-Subscription-Key": process.env.DELIJN_API_KEY,
+        },
+      }
+    )
+    .then(function (response) {
+      response.data.halteDoorkomsten[0].doorkomsten.forEach((doorkomst) => {
+        if (
+          doorkomst["dienstregelingTijdstip"] != undefined &&
+          doorkomst["real-timeTijdstip"] != undefined
+        ) {
+          let dTijd = Date.parse(doorkomst["dienstregelingTijdstip"]);
+          let rtTijd = Date.parse(doorkomst["real-timeTijdstip"]);
+          let delay = Math.abs(dTijd - rtTijd);
+          data.push(
+            `${Math.floor(delay / 1000 / 60)}:${Math.floor(
+              (delay / 1000) % 60
+            )}`
+          );
+        }
+      });
+      res.send(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
 });
 
 app.listen(port, () => {
