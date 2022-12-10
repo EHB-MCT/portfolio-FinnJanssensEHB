e822b9b27b666d8c25e4b69ce51553e0e2809cee
finnjanssensEHB <finn.janssens@student.ehb.be>
2022-12-10 10:32:05 +0100

    Add getCitiesForEntity function

---
+++ src/delijn.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+type City = {
+  cityNumber: number;
+  description: string;
+};
+
+export async function getCitiesForEntity(
+  entityNumber: number
+): Promise<City[]> {
+  let data = await axios({
+    url: `https://api.delijn.be/DLKernOpenData/v1/beta/entiteiten/${entityNumber}/gemeenten`,
+    method: "get",
+    headers: {
+      "Ocp-Apim-Subscription-Key": process.env.DELIJN_API_KEY,
+    },
+  });
+  let cities: City[] = data.data.gemeenten.map((city: any): City => {
+    return {
+      cityNumber: city.gemeentenummer,
+      description: city.omschrijving,
+    };
+  });
+  return cities;
+}
+
+export async function getStopsForCity(cityNumber: number) {
+  let data = await axios({
+    url: `https://api.delijn.be/DLKernOpenData/v1/beta/gemeenten/${cityNumber}/haltes`,
+    method: "get",
+    headers: {
+      "Ocp-Apim-Subscription-Key": process.env.DELIJN_API_KEY,
+    },
+  });
+  let stopNumbers: number[] = data.data.haltes.map((stop) => {
+    return Number(stop.haltenummer);
+  });
+  return stopNumbers;
+}
+
+export async function getBussesForStop(
+  entityNumber: number,
+  stopNumber: number
+) {
+  let data = await axios({
+    url: `https://api.delijn.be/DLKernOpenData/v1/beta/haltes/${entityNumber}/${stopNumber}/real-time`,
+    method: "get",
+    headers: {
+      "Ocp-Apim-Subscription-Key": process.env.DELIJN_API_KEY,
+    },
+  });
+  if (data.data.halteDoorkomsten[0]?.doorkomsten != undefined) {
+    data.data.halteDoorkomsten[0].doorkomsten?.map((stop) => {});
+  }
+}
