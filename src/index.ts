fa837aaae9399d48f673bdee683a27a9ab2376f4
finnjanssensEHB <finn.janssens@student.ehb.be>
2022-12-19 16:26:39 +0100

    Setup routes folder

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,6 @@
-import Bull from "bull";
 import express from "express";
-import {
-  getCitiesForEntity,
-  getBussesForStop,
-  getStopsForCity,
-} from "./delijn";
+import http from "http";
 
 const app = express();
-app.get("/cities", async function (req, res) {
-  if (!Number(req.query.entity)) {
-    res
-      .status(400)
-      .send(
-        `The request was not handled properly due to an invalid entity number: ${req.query.entity}`
-      );
-  } else {
-    const cities = await getCitiesForEntity(Number(req.query.entity));
-    res.json(cities);
-  }
-});
-
-app.listen(3001);
-
-// let TOTAL_DELAY: number = 0;
-
-// const delijnApiQueue = new Bull("delijnApiQueue");
-
-// delijnApiQueue.process(async (job, done) => {
-//   getBussesForStop(4, job.data.stopNumber);
-//   done();
-// });
-
-// const stopNumbers = await getStopsForCity(965);
-// stopNumbers.forEach((stopNumber, i) => {
-//   delijnApiQueue.add({
-//     stopNumber: stopNumber,
-//     jobIndex: i,
-//   });
-// });
+const server = new http.Server(app);
+server.listen(3030);
